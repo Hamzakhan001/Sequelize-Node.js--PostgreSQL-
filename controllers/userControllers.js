2604f09466e7c37627f14a51e166a8e345b1d28f
Muhammad Hamza khan <hamza.khan@gagamuller.com>
2023-04-07 22:48:54 +0500

    attributes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,7 @@
 
 const db=require('../models');
 const User=db.users;
+const {Sequelize}=require("sequelize");
 
 
 
@@ -14,4 +15,37 @@ let addUser=async (req,res)=>{
 	resp.status(200).json(resp) 
 }
 
-module.exports={addUser}
+
+let crudOperations=async (req,res)=>{
+	let dt=await User.update({name:"final"},{
+		where:{
+			id:3
+		}
+	})
+
+
+	let data=await User.bulkCreate([
+		{name:"hamza",email:"hamzawebdev@gmail.como",gender:"male"},
+		{name:"hamza",email:"hamzawebdev@gmail.como",gender:"male"},
+	])
+
+	//findOne,findAll
+}
+
+let queryData=async (req,res)=>{
+	await User.create({name:"hamza",email:"helloworl@gmail.com",gender:"male"},{fields:["email"]})
+
+	let data=await User.findAll({
+		attributes:[
+			'name',
+			['email','emailID'],
+			'gender',
+			[Sequelize.fn('Count',Sequelize.col('email'))]
+		]
+	});
+	 
+
+	//findOne,findAll
+}
+
+module.exports={addUser,crudOperations,queryData} 
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ require("./models/index");
 var userController=require("./controllers/userControllers")
 
 
+app.get("/add",userController.addUser);
+app.get("/crud",userController.crudOperations);
+app.get("/query",userController.queryData);
+
+
+
 app.listen(port,()=>{
 	console.log(`app listening`)
 })
